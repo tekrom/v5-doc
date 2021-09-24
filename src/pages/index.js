aab3151e3db7e887f556a889080982bd3ecefd31
Hasan ÜNAL <hasanunalweb@gmail.com>
2021-09-24 12:29:40 +0300

    home page updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,26 @@
 import React from 'react';
 import {
-  LightCta,
   Section,
-  SectionTitle,
-  Button,
   Text,
-  TextBlock,
-  Hero,
   SectionHeader,
-  LabelText,
-  TextBlocksGrid,
-  Card,
-  CardsGrid,
 } from '@algolia/ui-library';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-
-import showcaseProjects from './showcase-projects.json';
-import demoProjects from './demo-projects.json';
-import { DocSearchLogo } from '../components/DocSearchLogo';
 
 function Home() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <>
-      <Section>
+    /* ToDo: min vh değerleri gerekli */
+    <div className="uil-bgc-moon">
+      <Section className="min-vh-75">
         <SectionHeader title="v5 Döküman">
           <Text className="m-auto" style={{ maxWidth: '800px' }}>
-            We're kind of scratching our own itch here. As developers, we spend
-            a lot of time reading documentation, and it isn’t always easy to
-            find the information we need.
-          </Text>
-          <Text className="m-auto" style={{ maxWidth: '800px' }}>
-            No one's to blame, building a good search is a complex challenge. We
-            just happen to have a lot of experience doing that, and we want to
-            share it with the developer community.
+            açıklama metni
           </Text>
         </SectionHeader>
       </Section>
-
-    </>
+    </div>
   );
 }
 
