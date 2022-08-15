06845aabdf2ba87bcfdb7343479e18e3ce14c735
Hasan ÜNAL <hasanunalweb@gmail.com>
2022-08-15 16:34:13 +0300

    style and home page fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,15 @@ function Home() {
       <Section className="min-vh-75">
         <SectionHeader title="v5 Döküman">
           <Text className="m-auto" style={{ maxWidth: '800px' }}>
-            açıklama metni
+            <div>
+              Dökümana gitmek için <a href="/docs/tr-introduction">Tıkla</a>
+            </div>
+            <div>
+              Tema Marketine gitmek için <a href="https://tsoftthemes.com" target="_blank">Tıkla</a>
+            </div>
+            <div>
+              T-Soft ana sayfasına gitmek için <a href="https://tsoft.com.tr" target="_blank">Tıkla</a>
+            </div>
           </Text>
         </SectionHeader>
       </Section>
